9d4b5bb1f1f91e5ca570a5750f8da71be8de70dd
Cheng <g0927756978@gmail.com>
2026-03-11 16:01:58 +0800

    Update Lab02_Ex1.cpp

--- Lab02/Lab02_Ex1.cpp
+++ Lab02/Lab02_Ex1.cpp
@@ -1,13 +1,17 @@
 int findMax(const vector<int>& arr) {
 // TODO: Add counts for initialization
+// 1 step (array access)
 int max = arr[0];
 // TODO: Add counts for loop operations
+// 1 step (assignment)
 for (int i = 1; i < arr.size(); i++) {
 // TODO: Add counts for comparison and assignment
+// 1 step(Comparison)
 if (arr[i] > max) {
 max = arr[i];
 }
 }
 // TODO: Add counts for return
+// 2 step (array access + assignment)
 return max;
 }
