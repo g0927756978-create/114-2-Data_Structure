47670e14218ff9d10d4b2dcce35ecd5b19b34e7a
Cheng <g0927756978@gmail.com>
2026-03-11 15:46:13 +0800

    Add findMax function for maximum value in array
    Implement findMax function to find the maximum value in an array.

---
+++ Lab02/Lab02_Ex1.cpp
@@ -0,0 +1,13 @@
+int findMax(const vector<int>& arr) {
+// TODO: Add counts for initialization
+int max = arr[0];
+// TODO: Add counts for loop operations
+for (int i = 1; i < arr.size(); i++) {
+// TODO: Add counts for comparison and assignment
+if (arr[i] > max) {
+max = arr[i];
+}
+}
+// TODO: Add counts for return
+return max;
+}
