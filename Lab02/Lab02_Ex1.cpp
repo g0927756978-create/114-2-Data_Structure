194cf750a39cc411462c77f70f96d0a2b26a26fc
Cheng <g0927756978@gmail.com>
2026-03-11 20:58:17 +0800

    Update Lab02_Ex1.cpp

--- Lab02/Lab02_Ex1.cpp
+++ Lab02/Lab02_Ex1.cpp
@@ -1,17 +1,23 @@
 int findMax(const vector<int>& arr) {
-// TODO: Add counts for initialization
-// 1 step (array access)
-int max = arr[0];
-// TODO: Add counts for loop operations
-// 1 step (assignment)
-for (int i = 1; i < arr.size(); i++) {
-// TODO: Add counts for comparison and assignment
-// 1 step(Comparison)
-if (arr[i] > max) {
-max = arr[i];
+    // Counts for initialization: 2 step (array access + assignment)
+    int max = arr[0];
+    // Loop initialization: 1 step (assignment i = 1)
+    for (int i = 1; i < arr.size(); i++) {
+        // Comparison: 1 step (i < arr.size())  
+        // 2 step (array access + comparison)
+        if (arr[i] > max) {
+            // 2 step (array access + assignment)
+            max = arr[i];
+        }
+        // Increment: 1 step (i++)
+    }
+    // 1 step final failed comparison (loop exit)    
+    // Counts for return: 1 step (return)
+    return max;
 }
-}
-// TODO: Add counts for return
-// 2 step (array access + assignment)
-return max;
-}
+// Let n = arr.size()
+// Total operations (Worst-case):
+// 3 (initial) + 6 * (n - 1) (loop operations) + 1 (loop exit) + 1 (return)
+// = 3 + 6n - 6 + 2
+// = 6n - 1 operations
+// Therefore, O(n) complexity
